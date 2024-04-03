e9a6c33cc982d396cc525de0a0d77a450ef39c0c
Ikramul-m <i20727172@gmail.com>
2024-04-03 11:08:29 +0530

    lesson

--- hello.py
+++ hello.py
@@ -1,11 +1,10 @@
 def get_choice():
-    player_choice = "rock"
+    player_choice = input("Enter a choice (rock, paper, sicors): ")
     computer_choice = "paper"
-    choices = {"player": player_choice, "cpmouter": computer_choice}
+    choices = {"player": player_choice, "computer": computer_choice}
 
     return choices
 
 
 choice = get_choice()
 print(choice)
-
