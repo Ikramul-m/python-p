c09cd1701f6df4527af893d36adcd5c9111edc28
Ikramul-m <i20727172@gmail.com>
2024-04-03 14:10:02 +0530

    lesson

--- hello.py
+++ hello.py
@@ -1,6 +1,3 @@
-import random
-
-
 def get_choice():
     player_choice = input("Enter a choice (rock, paper, sicors): ")
     computer_choice = "paper"
@@ -14,10 +11,13 @@ print(choice)
 
 
 def check_win(player, computer):
-    print(f"You chose {player}")
-    if (player == computer):
-        return "It's a tie!"
+    print(f"You chose {player}, computer choose {computer}")
+    if player == "rock" and computer == "paper":
+        return "Rock shmashes sissor! You win!"
 
 
-ch = check_win("paper", "paper")
-print(ch)
+check_win("rock", "paper")
+
+k = check_win("rock", "paper")
+
+print(k)
