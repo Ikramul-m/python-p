ee5429dc45cf8af9484420afb7da57058d0d4e7a
Ikramul-m <i20727172@gmail.com>
2024-04-03 11:05:11 +0530

    lesson

--- hello.py
+++ hello.py
@@ -1,12 +1,11 @@
 def get_choice():
     player_choice = "rock"
     computer_choice = "paper"
+    choices = {"player": player_choice, "cpmouter": computer_choice}
 
-    return player_choice + " " + computer_choice
+    return choices
 
 
 choice = get_choice()
 print(choice)
 
-
-dict = {"name": "beu", "color": "blue"}
